769ead939ac3dd530768a342e4c0a9d0ea8e2ff8
Oleg Drozdov <syncopetwice@gmail.com>
2021-02-01 23:59:33 +0300

    Header
    Fixed header overflow issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectsComponent } from './projects/projects.component';
 
-
-const routes: Routes = [{
-  path: '',
-  component: ProjectsComponent
-}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
